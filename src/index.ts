bf1999da1df9ae63abbc5c2d77c2d09463b511f3
jatanolu <jacobrjean@gmail.com>
2023-11-16 11:25:15 +0100

    database and routes forming

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import app from './server'
+
+app.listen(3001, ()=>{
+  console.log("hello on port 3001")
+})
+// const http = require('http')
+//
+// const server = http.createServer(async (req,res) => {
+//   if (req.method === "GET" && req.url === "/") {
+//     res.statusCode= 200
+//     console.log('hello from server')
+//     res.end()
+//   }
+// })
+//
+//
+// server.listen(3001, () => {
+//   console.log("server on port 3001")
+// })
+//
