9cbfa88a6be81421e3e273c4bbd35ec09488af23
jatanolu <jacobrjean@gmail.com>
2023-11-20 15:47:29 +0100

    quick work on update handlers

--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { body, oneOf, validationResult } from "express-validator";
 import { handleInputErrors } from "./modules/middleware";
 import { createProduct, deleteProduct, getOneProduct, getProducts, updateProduct } from "./handlers/product";
+import { createUpdate, deleteUpdate, getOneUpdate, getUpdates, updateUpdate } from "./handlers/update";
 
 const router = Router();
 
@@ -11,18 +12,24 @@ router.put("/product/:id", body("name").isString(), handleInputErrors, updatePro
 router.post("/product", body("name").isString(), handleInputErrors, createProduct);
 router.delete("/product/:id", deleteProduct);
 
-router.get("/update", () => {});
-router.get("/update/:id", () => {});
+router.get("/update", getUpdates);
+router.get("/update/:id", getOneUpdate);
 router.put(
   "/update/:id",
   body("title").optional(),
   body("body").optional(),
   body("status").isIn(["IN_PROGRESS", "SHIPPED", "DEPRECATED"]),
   body("version").optional(),
-  (req, res) => {}
+  updateUpdate
 );
-router.post("/update", body("title").exists().isString(), body("body").exists().isString(), () => {});
-router.delete("/update/:id", () => {});
+router.post(
+  "/update",
+  body("title").exists().isString(),
+  body("body").exists().isString(),
+  body("productId").exists().isString(),
+  createUpdate
+);
+router.delete("/update/:id", deleteUpdate);
 
 router.get("/updatepoint", () => {});
 router.get("/updatepoint/:id", () => {});
@@ -32,11 +39,13 @@ router.put(
   body("description").optional().isString(),
   () => {}
 );
-router.post("/updatepoint",
+router.post(
+  "/updatepoint",
   body("name").isString(),
   body("description").isString(),
-  body('updateId').exists().isString(),
-  () => {});
+  body("updateId").exists().isString(),
+  () => {}
+);
 router.delete("/updatepoint/:id", () => {});
 
 export default router;
